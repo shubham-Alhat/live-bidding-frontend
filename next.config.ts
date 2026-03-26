706ad6694a2594af3bef4be664d567f023b643bd
shubham-Alhat <shubhamalhat520@gmail.com>
2026-03-26 16:11:15 +0530

    add API rewrites to forward requests to backend

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,15 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  async rewrites() {
+    return [
+      {
+        source: "/api/v1/:path*", // Match any route starting with /api/v1
+        destination:
+          "https://live-bidding-backend-kbv1.onrender.com/api/v1/:path*", // Forward to your backend
+      },
+    ];
+  },
 
   images: {
     remotePatterns: [
