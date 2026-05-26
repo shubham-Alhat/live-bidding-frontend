33980519742c8929cc729c93ac076a8be2d67adc
shubham-Alhat <shubhamalhat520@gmail.com>
2026-05-26 13:46:40 +0530

    feat: update logo image source in Navigation component

--- components/navigation.tsx
+++ components/navigation.tsx
@@ -15,7 +15,13 @@ export function Navigation() {
         {/* Logo */}
         <Link href="/home" className="flex items-center gap-2">
           <div className="flex h-8 w-8 items-center justify-center rounded-lg  text-primary-foreground font-bold bg-primary relative">
-            <Image src={"/logo.png"} fill alt="logo" />
+            <Image
+              src={
+                "https://res.cloudinary.com/diery17cm/image/upload/v1779783330/Doller_Icon_hpptyq.jpg"
+              }
+              fill
+              alt="logo"
+            />
           </div>
           <span className="hidden font-semibold text-foreground sm:inline">
             Bidhub
